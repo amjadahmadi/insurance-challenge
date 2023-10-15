bc2376e22315d349007f122dd9ea838074218be2
amjad <ahmadi_amjad@yahoo.com>
2023-10-16 00:14:06 +0330

    add tests

--- insurance/engine/tests.py
+++ insurance/engine/tests.py
@@ -1,20 +1,50 @@
-from django.test import TestCase, RequestFactory
-from .views import UserInsuranceView
+from django.test import TestCase
 from .helper import *
 
 
 class SimpleTest(TestCase):
     def setUp(self):
-
-        self.insurance = {
+        self.insurance1 = {
+            "age": 70,
+            "dependents": 2,
+            "house": {"ownership_status": "owned"},
+            "income": 0,
+            "marital_status": "married",
+            "risk_questions": [1, 1, 1],
+            "vehicle": {"year": 2018}
+        }
+        self.insurance2 = {
             "age": 35,
             "dependents": 2,
-            "house": None,
-            "income": 0,
+            "house": {"ownership_status": "mortgaged"},
+            "income": 100000,
             "marital_status": "married",
             "risk_questions": [0, 1, 0],
             "vehicle": {"year": 2018}
         }
+        self.insurance3 = {
+            "age": 20,
+            "dependents": 0,
+            "house": None,
+            "income": 250000,
+            "marital_status": "single",
+            "risk_questions": [0, 0, 0],
+            "vehicle": {"year": 2020}
+        }
+        self.insurance4 = {
+            "age": 45,
+            "dependents": 3,
+            "house": None,
+            "income": 100000,
+            "marital_status": "married",
+            "risk_questions": [1, 1, 0],
+            "vehicle": None
+        }
 
     def test_details(self):
-        print(calculate_all_insurance(self.insurance))
+        self.assertEqual(calculate_all_insurance(self.insurance1)['auto'], 'responsible')
+        self.assertEqual(calculate_all_insurance(self.insurance2)['home'], 'regular')
+        self.assertEqual(calculate_all_insurance(self.insurance3)['life'], 'economic')
+        self.assertEqual(calculate_all_insurance(self.insurance4)['auto'], 'ineligible')
+        self.assertEqual(calculate_all_insurance(self.insurance4)['disability'], 'regular')
+        self.assertEqual(calculate_all_insurance(self.insurance4)['life'], 'responsible')
